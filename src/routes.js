1bfa3cc2e07a27dc98fb393844a1417bd2aa44a4
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-12-01 11:30:20 -0300

    Updated App.js to routes.js pattern

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
+import Menu from "./components/Menu";
 
-function App() {
+function AppRoutes() {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/about" element={<About />} />
@@ -14,4 +16,4 @@ function App() {
   );
 }
 
-export default App;
+export default AppRoutes;
